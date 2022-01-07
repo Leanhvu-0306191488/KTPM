a77a89165b311e598d11bb7143b112513f99a99b
Leanhvu-0306191488 <62364561+Leanhvu-0306191488@users.noreply.github.com>
2022-01-07 13:42:14 +0700

    add CRUD

---
+++ KTPM/Migrations/20220107060911_new_database_1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KTPM.Migrations
+{
+    public partial class new_database_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
